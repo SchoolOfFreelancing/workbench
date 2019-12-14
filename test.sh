d3d2873792e837296e05d19e5f8c07bdb5e4eb68
Masum <schooloffreelancing@gmail.com>
2019-12-14 15:10:10 +0000

    first commit

---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+old_domain="wb.local" 
+new_domain="quantumcx.com"
+
+files=(
+    "compose/proxy.yaml"
+    "compose/backend.yaml"
+    "compose/app.yaml"
+    "config/trading-ui.env"
+    "compose/cryptonodes.yaml"
+    "config/integration/fixtures/barong.json"
+    "config/integration/fixtures/peatio.json"
+    "config/peatio.env"
+    "config/barong.env"
+    "config/barong/seeds.yml"
+    "config/toolbox.yaml"
+)
+
+for file in ${files[@]}; do
+    sed -i "s/${old_domain}/${new_domain}/g" ${file}
+done
+
